442f1dcf06cb1782afdce75d9bdcba0bc5f8872e
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-06-06 12:21:32 -0300

    :bug: fix: validation on image in cakes.schemas

--- src/schemas/cakes.schemas.js
+++ src/schemas/cakes.schemas.js
@@ -3,6 +3,6 @@ import joi from "joi"
 export const cakesSchema = joi.object({
     name: joi.string().min(2).required(),
     price: joi.number().positive().required(),
-    image: joi.string().required(), //rever
+    image: joi.string().regex(/\.com/).required(),
     description: joi.string()
 })
